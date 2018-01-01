781113e4bfb13259615a10efcdcf679205bed95b
Katriel Cohn-Gordon <me@katriel.co.uk>
2018-01-01 17:04:55 +0000

    start writing go

--- matasano.go
+++ matasano.go
@@ -1,7 +1,35 @@
-package main
+package matasano
 
-import "fmt"
+import b64 "encoding/base64"
+import hex "encoding/hex"
+import "log"
 
-func main() {
-	fmt.Printf("Hello, world!\n")
+// decode a hex string into a byte array
+func bytes_of_hex(s string) []byte {
+	decoded, err := hex.DecodeString(s)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return decoded
+}
+
+// encode a byte array into b64
+func b64_of_bytes(b []byte) string {
+	return b64.StdEncoding.EncodeToString(b)
+}
+
+// convert hex to b64 via bytes
+func b64_of_hex(s string) string {
+	return b64_of_bytes(bytes_of_hex(s))
+}
+
+// xor two byte arrays
+func xor(a, b []byte) []byte {
+	n := len(a)
+	if n > len(b) { n = len(b) }
+	dst := make([]byte, n)
+	for i := 0; i < n; i++ {
+		dst[i] = a[i] ^ b[i]
+	}
+	return dst
 }
---
+++ matasano_test.go
@@ -0,0 +1,32 @@
+package matasano
+
+import "bytes"
+import "testing"
+
+func Test_1_1(t *testing.T) {
+	cases := []struct {
+		in, want string
+	}{
+		{"49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d", "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"},
+		{"", ""},
+	}
+	for _, c := range cases {
+		got := b64_of_hex(c.in)
+		if got != c.want {
+			t.Errorf("b64_of_hex(%q) == %q, want %q",
+				c.in,
+				got,
+				c.want)
+		}
+	}
+}
+
+func Test_1_2(t *testing.T) {
+	x := bytes_of_hex("1c0111001f010100061a024b53535009181c")
+	y := bytes_of_hex("686974207468652062756c6c277320657965")
+	z:= bytes_of_hex("746865206b696420646f6e277420706c6179")
+
+	if !bytes.Equal(xor(x, y), z) {
+		t.Errorf("xoring gave the wrong answer")
+	}
+}
